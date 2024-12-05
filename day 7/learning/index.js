a51e729ac328a33fc651298b603cfceea120060e
anil6267 <anilahirwer28@gmail.com>
2024-12-05 10:03:14 +0530

    Add files via upload

---
+++ day 7/learning/index.js
@@ -0,0 +1,12 @@
+console.log("----welcome in js-----")
+const title = document. getElementById("title");
+console.log(title);
+
+title.innerText="hi i am jaya";
+
+
+const heading = document.getElementsByClassName("heading");
+
+heading[0].innerText = "First world";
+ heading[1].innerText = "Second world";
+ console.log(heading);
